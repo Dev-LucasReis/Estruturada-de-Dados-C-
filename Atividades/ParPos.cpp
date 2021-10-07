bce317282a1c55db30c31c9776a607e2ae9bbc88
LucasReis <lucasgja2014@gmail.com>
2021-10-07 17:52:02 -0300

    Update ParPos.cpp

--- Atividades/ParPos.cpp
+++ Atividades/ParPos.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
 
-bool VerificaPosPar(int n){
+class PosPar{
+	
+	
+	
+	public:
+		int n;
+	
+	void entrada(){
+		cout <<"Digite um numero:" <<"\n";
+		cin >> n ;
+	}
+	
+	bool VerificaPosPar(){
 	
 	if(n >= 0 && n % 2 == 0){
 		return true;
@@ -12,32 +24,37 @@ bool VerificaPosPar(int n){
 	}
 }
 
-int cont(int n){
+int cont(){
 	
-	int aux = 0;
+	int aux = 1;
 	
-	while(VerificaPosPar(n) == true){
+	while(VerificaPosPar() == true){
 		
-		cout << "Digite um numero: " << "\n";
-	    cin >> n;
+		entrada();
 
         aux = aux + 1;
-        
-        if(VerificaPosPar(n) == false){
         	
-        	aux = aux - 1;
+		}if(VerificaPosPar() == false){
+			
+			aux = aux - 1;
 		}
+		
+		return aux;
 	}
 	
-	return aux;
-}
+	
+
+};
+
+
 
 int main(){
 	
-int n;
-	
-	cout << "\n";
-	cout << "Quantidade de numeros pares e positivos: " << "\n" << cont(n);
+PosPar obj;
+obj.entrada();
+cout<<"\n";
+cout<< obj.VerificaPosPar() << "\n";
+cout << "Quantidade de numeros pares e positivos: " << "\n" <<obj.cont();
 	
 	return 0;
 }
